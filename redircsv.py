2069cfaeb3946333a935b23c87016e5d279cec68
Michiel Maas <michieljmmaas@gmail.com>
2020-04-05 08:04:49 +0200

    reset to working again

--- redircsv.py
+++ redircsv.py
@@ -4,9 +4,10 @@ import pandas as pd
 def rereadFiles(files):
     for file in files:
         file_string = file
-        dataframe = pd.read_csv(file_string)
-        dataframe.set_index("Category", inplace=True)
+        dataframe = pd.read_csv(file_string, index_col="Category", sep=';')
+        print(dataframe)
+        # dataframe.set_index("Category", inplace=True)
         # dataframe.drop(dataframe.columns[[0]], axis=1, inplace=True)
-        dataframe = dataframe[['Category', 'Aantal per 100.000 inwoners', 'Aantal']]
+        # dataframe = dataframe[['"Category"', '"Aantal per 100.000 inwoners"', '"Aantal"']]
         # dataframe[0]['Category'] = 's-Gravenhage'
         dataframe.to_csv(file_string)
